1c7681fcc5e4d916bebbd4836b7ff0f262363ac4
Alexa Billings <alexa.billings@gmail.com>
2018-03-06 09:38:22 -0600

    bus overview responsive

--- src/BusinessOverview.js
+++ src/BusinessOverview.js
@@ -5,7 +5,7 @@ let businessOverview = props => {
   return (
     <div className="bus-overview">
     <h1>Personalized Assistance for Independent Living</h1>
-    <p>Ro on the Go, LLC., was formed out of a need I saw when taking care of my aging parents. I was one of four siblings and on certain days, we wished for a fifth sibling. Someone to help us out with doctor visits, take dad in for chemo treatments, or mom in for blood transfusions.
+    <p>Ro on the Go, LLC., was formed out of a need I saw when taking care of my aging parents. I was one of four siblings and on certain days, we wished for a fifth sibling - someone to help us out with doctor visits, take dad in for chemo treatments, or mom in for blood transfusions.
   In today’s society, families are spread far and wide, leaving individuals of all ages who are new to a community without a network of friends or family who can provide assistance. I would like to be part of your network in the Grand Island community.</p>
   </div>
   )
