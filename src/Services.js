e55a28eae89eb15f2366949b7d23573af7acc463
Alexa Billings <alexa.billings@gmail.com>
2018-03-06 12:12:39 -0600

    more styling

--- src/Services.js
+++ src/Services.js
@@ -12,25 +12,18 @@ let Services = props => {
     <div className="services-page">
       <h1 className="heading">Services</h1>
         <div className="services-text-icons-container">
-          <div className="column-item">
-            <img src={carIcon} className="services-icon" alt="car-icon" />
-              <div className="text">
-                <h2>Outpatient Surgeries & Medical Procedures Requiring Sedation</h2>
-                <p>I provide transportation to and from outpatient surgeries and medical procedures requiring sedation. Post-surgery or procedure, I can stay with you until the end of the day, using that time to prepare meals or help around the house, making your recovery more comfortable.</p>
-              </div>
-          </div>
-        <div className="column-item">
-          <img src={shoppingIcon} className="services-icon" alt="shopping-icon" />
-            <div className="text">
-              <h2>Personal Shopping</h2>
-              <p>If you would like me to do your grocery shopping or clothes shopping, I can pick items up for you, or I can take you to the store to make your own selections.</p>
-            </div>
-       </div>
         <div className="column-item">
           <img src={mealPrepIcon} className="services-icon" alt="meal-prep-icon" />
             <div className="text">
               <h2>Meal Preparation</h2>
               <p>Meal preparation is something I do to relax; I love to cook. I have experience with a range of alternative diets including gluten-free, dairy-free and diabetic. This service is ideal for clients undergoing surgery, new moms and families, or anyone needing dietary assistance.</p>
+            </div>
+        </div>
+        <div className="column-item">
+          <img src={billPayingIcon} className="services-icon" alt="bill-pay-icon" />
+            <div className="text">
+              <h2>Bill Paying</h2>
+              <p>Assistance for individuals whose vision or cognition makes it dffcult for them to pay their own bills.</p>
             </div>
         </div>
         <div className="column-item">
@@ -41,12 +34,19 @@ let Services = props => {
             </div>
         </div>
         <div className="column-item">
-          <img src={billPayingIcon} className="services-icon" alt="bill-pay-icon" />
+          <img src={shoppingIcon} className="services-icon" alt="shopping-icon" />
             <div className="text">
-              <h2>Bill Paying</h2>
-              <p>Assistance for individuals whose vision or cognition makes it dffcult for them to pay their own bills.</p>
+              <h2>Personal Shopping</h2>
+              <p>If you would like me to do your grocery shopping or clothes shopping, I can pick items up for you, or I can take you to the store to make your own selections.</p>
             </div>
-        </div>
+       </div>
+          <div className="column-item">
+            <img src={carIcon} className="services-icon" alt="car-icon" />
+              <div className="text">
+                <h2>Outpatient Surgeries & Medical Procedures Requiring Sedation</h2>
+                <p>I provide transportation to and from outpatient surgeries and medical procedures requiring sedation. Post-surgery or procedure, I can stay with you until the end of the day, using that time to prepare meals or help around the house, making your recovery more comfortable.</p>
+              </div>
+          </div>
         <div className="column-item">
           <img src={helpingHandIcon} className="services-icon" alt="helping-hand-icon" />
             <div className="text">
